c8806c9ba817a7c7814e7426b4bda938182b026e
zxFiro <kupp0100@gmail.com>
2023-01-20 19:25:24 -0300

    added an example to cast the query JSON to the excercise type

--- src/components/lvltutor/Tools/DynamicQuery.tsx
+++ src/components/lvltutor/Tools/DynamicQuery.tsx
@@ -1,19 +1,25 @@
 import { useGQLQuery } from "rq-gql";
-import { gql } from "../../../graphql";
+import { gql, PotatoqueryQuery } from "../../../graphql";
 import { Button, Input,Text } from "@chakra-ui/react";
 import { useEffect,useState,useRef } from "react";
+import type {ExType} from "./ExcerciseType";
+
+const ParsedQuery=({obj}:{obj:PotatoqueryQuery})=>{
+    const [potatoQ,setPotatoQ] = useState<ExType | null>();
+    useEffect(()=>{
+        let value:Object| unknown=(obj?.contentByCode?.json)
+        if(value){
+            let a=value as ExType;
+            setPotatoQ(a);
+        }
+    },[obj])
+    return (potatoQ?<Text>{JSON.stringify(potatoQ.steps[0].answers[0])}</Text>:null)
+}
 
 const DynamicQuery=()=>{
-    const [text,setText]=useState<string>("")
+    const [text,setText]=useState<string>("");
     const [submit,setSubmit]=useState(false);
     const sText=useRef<string>("");
-
-    useEffect(()=>{
-        if(submit){
-            sText.current=text;
-            setSubmit(false);
-        }
-    },[submit])
 
     const { data: potato } = useGQLQuery(gql(/* GraphQL */ `
     query potatoquery($code:String!){
@@ -23,11 +29,18 @@ const DynamicQuery=()=>{
     }
     `),{code:sText.current});
 
+    useEffect(()=>{
+        if(submit){
+            sText.current=text;
+            setSubmit(false);
+        }
+    },[submit])
+
     return(
         <>
             <Input onChange={(e)=>{setText(e.target.value)}}/>
             <Button onClick={()=>{setSubmit(true)}}>Consultar</Button>
-            <Text>{JSON.stringify(potato?.contentByCode?.json)}</Text>
+            {potato?<ParsedQuery obj={potato}/>:null}
         </>
     )
 }
