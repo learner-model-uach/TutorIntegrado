6bd3dca4b604d396d7c10efd62b11edd12cf7cc1
miguel262 <miguel.nahuelpan@alumnos.uach.cl>
2023-01-25 17:10:47 -0300

    update navegation, selectByCode para admin

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -8,45 +8,46 @@ import { SidebarLink } from "./SidebarLink";
 
 export function Navigation() {
   const { user } = useAuth();
+
+  const admin = (user?.role ?? "") == "ADMIN" ? true : false;
   return (
     <ScrollArea pt="5" pb="6">
       <Stack pb="6">
         <SidebarLink icon={<FaHome />} href="/">
           Inicio
         </SidebarLink>
-        <Text fontWeight="black">Selección por Código</Text>
-        <SidebarLink icon={<FaSearch />} href={"selectByCode"}>
-          Search by Code
-        </SidebarLink>
+        {user && admin && (
+          <>
+            <Text fontWeight="black">Selección por Código</Text>
+            <SidebarLink icon={<FaSearch />} href={"selectByCode"}>
+              Search by Code
+            </SidebarLink>
+          </>
+        )}
       </Stack>
 
       <Stack pb="6">
         {user && (
           <>
-            <Text fontWeight="black">Rudimentos Algebraicos</Text>
-
-            <SidebarLink href={"contentSelect?topic=16,4,3,5,6,7,8&registerTopic=16" /*17,18*/}>
+            <Text fontWeight="black">Tópicos</Text>
+            <SidebarLink
+              href={"contentSelect?topic=16,4,3,5,6,7,8,17,18&registerTopic=16" /*17,18*/}
+            >
               Rudimentos Algebraicos
             </SidebarLink>
-            <SidebarLink href={"contentSelect?topic=5"}>Factor Común Compuesto</SidebarLink>
-            <SidebarLink href={"contentSelect?topic=6"}>Diferencia de Cuadrados</SidebarLink>
-            <SidebarLink href={"contentSelect?topic=7"}>Diferencia y Suma de Cubos</SidebarLink>
-            <SidebarLink href={"contentSelect?topic=8"}>Trinomios Cuadraticos</SidebarLink>
-            <Text fontWeight="medium">-Fracción Algebraica</Text>
-            <SidebarLink href={"contentSelect?topic=9"}>Operatoria de Fracciones</SidebarLink>
-            <SidebarLink href={"contentSelect?topic=10"}>Suma de Fracciones MCM</SidebarLink>
-            <Text fontWeight="black">Ecuaciones</Text>
-            <SidebarLink href={"contentSelect?topic=11"}>Ecuaciones</SidebarLink>
-            <SidebarLink href={"contentSelect?topic=12"}>Sistema de ecuaciones</SidebarLink>
-            <Text fontWeight="black">Potencias y Raices</Text>
-            <SidebarLink href={"contentSelect?topic=13"}>Notación Cientifica</SidebarLink>
-            <SidebarLink href={"contentSelect?topic=14"}>Evaluar Expresión</SidebarLink>
-            <SidebarLink href={"contentSelect?topic=15"}>Reducción de Expresión</SidebarLink>
-            <SidebarLink href={"contentSelect?topic=16"}>Racionalización</SidebarLink>
-            <Text fontWeight="black">Geometría</Text>
-            <SidebarLink href={"contentSelect?topic=17"}>Área y Perímetro</SidebarLink>
-            <SidebarLink href={"contentSelect?topic=18"}>Teorema de Thales</SidebarLink>
-            <SidebarLink href={"contentSelect?topic=19"}>Teorema de Pitágoras</SidebarLink>
+            <SidebarLink href={"contentSelect?topic=24,25,26&registerTopic=24" /*17,18*/}>
+              Ecuaciones
+            </SidebarLink>
+            <SidebarLink
+              href={
+                "contentSelect?topic=19,20,21,22,23,8&registerTopic=19" /*hay 2 topicos padres*/
+              }
+            >
+              Potencias y raíces
+            </SidebarLink>
+            <SidebarLink href={"contentSelect?topic=16,4,3,5,6,7,8&registerTopic=16" /*17,18*/}>
+              Geometría
+            </SidebarLink>
           </>
         )}
       </Stack>
