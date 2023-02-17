5febed21830ad533846889658f200cfb1d399278
Marcelo Trivinos <marceloto260@gmail.com>
2023-02-17 02:50:33 -0300

    Se corrigio requestHint y el comportamiento del componente Hint
    El componente Hint tenia algunos problemas.
    1) El request Hint usaba count + 1, el valor de count era el valor que
       tenia el estado al momento de renderizar el componente y no al momento
       de actualizarlo (es decir, al momento de usar useState), esto provocaba
       que si el boton hint ya no tenia mas hint que mostrar y se habria
       nuevamente el hint entonces count + 1 daba un valor de indice mas
       grande que el arreglo de los hints relacionados a la respuesta del usuario
       (ya que al renderizar el componente se actualiza con el valor de count del
       momento anterior).
    2) Luego estaba el problema de la condicion de carrera, ya que se necesitaba
       asegurar que todas respuestas relacionasdas a la respuesta del
       usuario estuvieran definidas al momento del renderizado, pero la
       variable al momento del renderizado estaba vacia (ya que el useState
       actualiza la variable, pero el valor de la variable actualizada solo
       es notada por el return que termina dentro de la funcion render, pero
       no por el resto del componente, el resto del componente accede al
       valor de las variables al momento de renderizarlo).

--- src/components/tutorEcuaciones/Panels/Hint.jsx
+++ src/components/tutorEcuaciones/Panels/Hint.jsx
@@ -32,27 +32,49 @@ export const Hint = ({
   setNewHintAvaliable,
   newHintAvaliable,
   answerId, // id the answer
-  nStep, // "stepId" field defined in the json file
-  code, // "code" field defined in the json file
+  nStep, // "stepId" field defined in the exercise
+  code, // "code" field defined in the exercise
   setHintsShow // number of times a hint has been shown
 }) => {
   const startAction = useAction({});
   const initialFocusRef = useRef();
 
-  const [allHints, setAllHints] = useState(hints); // all the hints of the step
-  const [count, setCount] = useState(-1); // count of matchingError
-  const [countHint, setCountHint] = useState(-1); // counts the number of accumulated hints displayed to the user
+  const [allHints, setAllHints] = useState([]); // all the hints of the step
+  const [countHint, setCountHint] = useState(-1); // index of the element of hintsAvaliableList that the user can currently see
   const [countNotification, setCountNotication] = useState(0);
   const [disabledHint, setDisabledHint] = useState(firstTimeHint); // configure if the button is disabled or not
   const [hintsAvaliableList, setHintsAvaliableList] = useState([]); // accumulated hints displayed to the user
   const [shake, setShake] = useState(false);
+  const hintIndex = useRef(-1); // this is used to keep the index of the possible answers that the user is seeing
+  const hintsAssociatedAnswer = useRef([]); // all hints associated with this idAnswer (both non-generic and generic)
+  const [updateAssociatedAnswer, setUpdateAssociatedAnswer] = useState(false);
   
   useEffect(() => {
-    setCount(-1); // number of hints shown to the user
-    setCountHint(-1); // number of hints available to display to the user
+    setCountHint(-1);
     setAllHints(hints);
     setHintsAvaliableList([]);
-  }, [answerId]);
+    hintIndex.current = -1;
+	
+	// function setAllHints() must be executed every time the user changes
+	// answer (the id of the answer defined in the exercise is used, if
+	// there is no answer defined in the exercise then it defaults to 0,
+	// this is to guarantee that it shows the generic answers, and this is
+	// defined in the three panel components that are parents of this
+	// component) or advances one step in the exercise and
+	// hintsAssociatedAnswer needs to update the data every time the user
+	// changes answer or advances one step in the exercise, but
+	// hintsAssociatedAnswer must be updated after setAllHints()
+	// has been updated (since getAllHints uses allHints), to
+	// avoid this race condition this updateAssociatedAnswer is used.
+	setUpdateAssociatedAnswer(true);
+  }, [answerId, nStep]);
+  
+  // ensures that allHints is defined before executing getAllHints
+  // (avoid race condition)
+  useEffect(() => {
+    hintsAssociatedAnswer.current = getAllHints(answerId);
+	setUpdateAssociatedAnswer(false);
+  }, [updateAssociatedAnswer]);
   
   useEffect(() => {
     if (getHint(answerId)) {
@@ -61,8 +83,8 @@ export const Hint = ({
       setTimeout(() => setShake(false), 2000);
       if (newHintAvaliable) {
         setCountNotication(1);
-      }
-    }
+      };
+	}
   }, [newHintAvaliable]);
 
   const getHint = (idAnswer) => {
@@ -75,51 +97,86 @@ export const Hint = ({
     }
     return null;
   };
+  
+  // returns all hints that can be associated with the user's
+  // response (both non-generic and generic).
+  const getAllHints = (idAnswer) => {
+    if (allHints != undefined) {
+      let filterHint = allHints.filter(hint => {
+        return hint.answers.includes(idAnswer);
+      });
+      if (filterHint != undefined) {
+        filterHint = filterHint.concat(allHints.filter(hint => hint.generic && !filterHint.includes(hint)));
+	  }
+	  else {
+        filterHint = filterHint.concat(allHints.filter(hint => hint.generic));
+	  }
+      return filterHint;
+    }
+    return null;
+  }
+  
+  // The following three functions modify the index with which,
+  // together with the getAllHints function, they are used to
+  // choose the id of the answer that the user is seeing.
+  const nextHintIndex = () => {
+    hintIndex.current += 1;
+  }
+
+  const backHintIndex = () => {
+    hintIndex.current -= 1;
+  }
+  
+  const openHintIndex = () => {
+    hintIndex.current += 1;
+  }
 
   const handOnClickNext = (e) => {
+    nextHintIndex();
     startAction({
       verbName: "requestHint",
       stepID: nStep,
       contentID: code,
-      hintID: count + 1,
+      hintID: hintsAssociatedAnswer.current[hintIndex.current].id,
       extra: { open: "next" },
     });
-    setCount((prev) => prev + 1);
-    setCountHint((prev) => prev + 1);
+	setCountHint((prev) => prev + 1);
   };
 
   const handOnClickBack = (e) => {
+    backHintIndex();
     startAction({
       verbName: "requestHint",
       stepID: nStep,
       contentID: code,
-      hintID: count - 1,
+      hintID: hintsAssociatedAnswer.current[hintIndex.current].id,
       extra: { open: "prev" },
     });
-    setCount((prev) => prev - 1);
-    setCountHint((prev) => prev - 1);
+	setCountHint((prev) => prev - 1);
   };
 
   const handOnClickHint = (e) => {
     setCountNotication(0);
-    if (newHintAvaliable) {
-      startAction({
-        verbName: "requestHint",
-        stepID: nStep,
-        contentID: code,
-        hintID: count + 1,
-        extra: { open: "new" },
-      });
-      let newHint = getHint(answerId);
-      if(newHint) {
+
+	let newHint = getHint(answerId);
+    if(newHint) {
+	  if (newHintAvaliable) {
         setHintsAvaliableList(prev => [...prev, newHint]);
         setAllHints(prev => prev.filter(hint => hint.id !== newHint.id));
-        setCountHint(prev => prev + 1);
-      }
-      setCount(prev => prev + 1);
-      setNewHintAvaliable(false);
+		openHintIndex();
+        setCountHint((prev) => prev + 1)
+	  }
       setHintsShow((prev) => prev + 1);
+      setNewHintAvaliable(false);  
     }
+	
+	startAction({
+      verbName: "requestHint",
+      stepID: nStep,
+      contentID: code,
+      hintID: hintsAssociatedAnswer.current[hintIndex.current].id,
+      extra: { open: "new" },
+    });
   };
 
   return (
