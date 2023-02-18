54aed3aca868ba905a9430d749e1138b968edc66
Pablo Sáez <pablosaez1995@gmail.com>
2023-02-18 11:32:51 -0300

    remove alias

--- src/components/contentSelectComponents/CardSelectionDynamic.tsx
+++ src/components/contentSelectComponents/CardSelectionDynamic.tsx
@@ -2,11 +2,9 @@ import dynamic from "next/dynamic";
 import type { ComponentProps } from "react";
 import type { CardSelection } from "./CardSelection";
 
-const CardSelectionDynamic = dynamic<ComponentProps<typeof CardSelection>>(
+export const CardSelectionDynamic = dynamic<ComponentProps<typeof CardSelection>>(
   () => import("./CardSelection").then(v => v.CardSelection),
   {
     ssr: false,
   },
 );
-
-export { CardSelectionDynamic as CardSelection };
