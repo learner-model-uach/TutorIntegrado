57ba9db5ca1963e03fedcef3f5831275bcfdc350
Firo Losstarot <Kupp0100@gmail.com>
2023-12-12 06:09:18 -0300

    fixed some issues generated after changing the interface used inside handleanswer in mq2

--- src/utils/MQPostfixSolver.tsx
+++ src/utils/MQPostfixSolver.tsx
@@ -1,84 +1,85 @@
 import MQProxy from "../components/lvltutor/Tools/MQProxy";
 
-interface value {
-  name: String;
-  value: Number;
+interface value{
+  name: String, value: Number 
 }
 
-interface values {
-  values: Array<value>;
+interface values{
+  values:Array<value>
 }
-const MQPostfixSolver = (MQPostfixExpression: string, ValuesObject: values) => {
-  const valueReplace = (input: string, values: values) => {
-    let l = values.values.length;
-    let output = input;
-    for (let i = 0; i < l; i++) {
-      let value = values.values[i];
-      if (!value) continue;
-      output = output.replaceAll(" " + value.name, " " + value.value);
-    }
-    return output;
-  };
 
-  const solveExpresion = (expression: String) => {
-    let exp = expression.split(" ");
-    let stack: number[] = [];
-    let alphabet = new RegExp(/^[a-zA-Z]$/);
-    let number = new RegExp(/^[0-9]+[.0-9]*$/);
-    for (let i = 0; i < exp.length; i++) {
-      let value = exp[i];
-      if (!value) continue;
-      if (value.length == 1 && alphabet.test(value)) {
-        stack.push(1);
-      } else if (number.test(value)) {
-        stack.push(parseFloat(value));
-      } else {
-        if (value.localeCompare("/") == 0) {
-          let a: number = stack.pop()!;
-          let b: number = stack.pop()!;
-          stack.push(b / a);
-        } else if (value.localeCompare("*") == 0) {
-          let a: number = stack.pop()!;
-          let b: number = stack.pop()!;
-          stack.push(b * a);
-        } else if (value.localeCompare("sqrt") == 0) {
-          let a: number = stack.pop()!;
-          stack.push(Math.sqrt(a));
-        } else if (value.localeCompare("sin") == 0) {
-          let a: number = stack.pop()!;
-          stack.push(Math.sin(a));
-        } else if (value.localeCompare("cos") == 0) {
-          let a: number = stack.pop()!;
-          stack.push(Math.cos(a));
-        } else if (value.localeCompare("^") == 0) {
-          let a: number = stack.pop()!;
-          let b: number = stack.pop()!;
-          stack.push(b ** a);
-        } else if (value.localeCompare("+") == 0) {
-          let a: number = stack.pop()!;
-          let b: number = stack.pop()!;
-          stack.push(b + a);
-        } else if (value.localeCompare("-") == 0) {
-          let a: number = stack.pop()!;
-          let b: number = stack.pop()!;
-          stack.push(b - a);
-        } else if (value.localeCompare("\\um") == 0) {
-          let a: number = stack.pop()!;
-          stack.push(-a);
-        }
+const valueReplace = (input: string, values: values) => {
+  let l = values.values.length;
+  let output = input;
+  for (let i = 0; i < l; i++) {
+    let value = values.values[i];
+    if (!value) continue;
+    output = output.replaceAll(" " + value.name, " " + value.value);
+  }
+  return output;
+};
+
+const solveExpresion = (expression: String) => {
+  let exp = expression.split(" ");
+  let stack: number[] = [];
+  let alphabet = new RegExp(/^[a-zA-Z]$/);
+  let number = new RegExp(/^[0-9]+[.0-9]*$/);
+  for (let i = 0; i < exp.length; i++) {
+    let value = exp[i];
+    if (!value) continue;
+    if (value.length == 1 && alphabet.test(value)) {
+      stack.push(1);
+    } else if (number.test(value)) {
+      stack.push(parseFloat(value));
+    } else {
+      if (value.localeCompare("/") == 0) {
+        let a: number = stack.pop()!;
+        let b: number = stack.pop()!;
+        stack.push(b / a);
+      } else if (value.localeCompare("*") == 0) {
+        let a: number = stack.pop()!;
+        let b: number = stack.pop()!;
+        stack.push(b * a);
+      } else if (value.localeCompare("sqrt") == 0) {
+        let a: number = stack.pop()!;
+        stack.push(Math.sqrt(a));
+      } else if (value.localeCompare("sin") == 0) {
+        let a: number = stack.pop()!;
+        stack.push(Math.sin(a));
+      } else if (value.localeCompare("cos") == 0) {
+        let a: number = stack.pop()!;
+        stack.push(Math.cos(a));
+      } else if (value.localeCompare("^") == 0) {
+        let a: number = stack.pop()!;
+        let b: number = stack.pop()!;
+        stack.push(b ** a);
+      } else if (value.localeCompare("+") == 0) {
+        let a: number = stack.pop()!;
+        let b: number = stack.pop()!;
+        stack.push(b + a);
+      } else if (value.localeCompare("-") == 0) {
+        let a: number = stack.pop()!;
+        let b: number = stack.pop()!;
+        stack.push(b - a);
+      } else if (value.localeCompare("\\um") == 0) {
+        let a: number = stack.pop()!;
+        stack.push(-a);
       }
     }
-    MQProxy.finishedEval = stack.length == 1 ? true : false;
-    return stack[0];
-  };
+  }
+  MQProxy.finishedEval = (stack.length == 1) ? true : false;
+  return stack[0];
+};
 
-  const solve = (input: string, values: values | undefined) => {
-    let a = input;
+const solve = (input: string, values: values|undefined) => {
+  let a = input;
 
-    if (values != undefined && values.values.length > 0) a = valueReplace(a, values);
-    return solveExpresion(a.substring(1));
-  };
+  if (values != undefined && values.values.length > 0) a = valueReplace(a, values as values);
+  return solveExpresion(a.substring(1));
+};
 
+const MQPostfixSolver = (MQPostfixExpression: string, ValuesObject: values|undefined) => {
   return solve(MQPostfixExpression, ValuesObject);
 };
+
 export default MQPostfixSolver;
