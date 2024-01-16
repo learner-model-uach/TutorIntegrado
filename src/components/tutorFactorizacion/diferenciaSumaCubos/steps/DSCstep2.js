de5109780d39b0982fbd38738ff622fc8c068752
miguel262 <miguel.nahuelpan@alumnos.uach.cl>
2024-01-16 11:19:58 -0300

    update tutorFac (#73)

--- src/components/tutorFactorizacion/diferenciaSumaCubos/steps/DSCstep2.js
+++ src/components/tutorFactorizacion/diferenciaSumaCubos/steps/DSCstep2.js
@@ -29,8 +29,28 @@ export const DSCstep2 = ({
     //contador de intentos
     setAttempts(attempts + 1);
     const responseStudent = [
-      response1.current.value.replace(/[*]|[(]|[)]|[{]|[}]| /g, "").toLowerCase(),
-      response2.current.value.replace(/[*]|[(]|[)]|[{]|[}]| /g, "").toLowerCase(),
+      response1.current.value
+        .replace(/[*]|[(]|[)]|[{]|[}]| /g, "")
+        .replace(/[²]| /g, "^2")
+        .replace(/[³]| /g, "^3")
+        .replace(/[⁴]| /g, "^4")
+        .replace(/[⁵]| /g, "^5")
+        .replace(/[⁶]| /g, "^6")
+        .replace(/[⁷]| /g, "^7")
+        .replace(/[⁸]| /g, "^8")
+        .replace(/[⁹]| /g, "^9")
+        .toLowerCase(),
+      response2.current.value
+        .replace(/[*]|[(]|[)]|[{]|[}]| /g, "")
+        .replace(/[²]| /g, "^2")
+        .replace(/[³]| /g, "^3")
+        .replace(/[⁴]| /g, "^4")
+        .replace(/[⁵]| /g, "^5")
+        .replace(/[⁶]| /g, "^6")
+        .replace(/[⁷]| /g, "^7")
+        .replace(/[⁸]| /g, "^8")
+        .replace(/[⁹]| /g, "^9")
+        .toLowerCase(),
     ];
     const validate = element =>
       element[0] === responseStudent[0] && element[1] === responseStudent[1];
