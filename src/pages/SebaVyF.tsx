65b0ca1996da8dbd9e4ed990437a9f324e620b0d
NicoleNA1101 <82178025+NicoleNA1101@users.noreply.github.com>
2024-10-01 00:36:34 -0300

    Update SebaVyF.tsx con Latex

--- src/pages/SebaVyF.tsx
+++ src/pages/SebaVyF.tsx
@@ -1,73 +1,69 @@
-import { Select, Box, Button, Input, Flex, Text, Stack, Checkbox } from '@chakra-ui/react';
+import { Select, Box, Button, Input, Flex, Text, Stack, Checkbox, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, useDisclosure } from '@chakra-ui/react';
 import { useState } from 'react';
-
+import { MathJaxContext, MathJax } from 'better-react-mathjax';
 
 export default function newExercise() {
-  // Estado inicial: tarjeta de tipo "enunciado"
   const [cards, setCards] = useState([
     { type: 'enunciado', title: '', question: '', expression: '', summary: '',
-      successMessage: '', trueOption: false, falseOption: false, }]);
+      successMessage: '', trueOption: false, falseOption: false, isEditingExpression: false }
+  ]);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [currentCardIndex, setCurrentCardIndex] = useState(null);
+
+  const operations = [
+    { label: '+', command: '+' },
+    { label: '-', command: '-' },
+    { label: '\\times', command: '\\times' },
+    { label: '\\div', command: '\\div' },
+    { label: '\\frac{□}{□}', command: '\\frac{}{}' },
+    // Otros símbolos que desees agregar...
+  ];
 
   const addCard = () => {
-    // Agregar siempre una tarjeta de tipo "paso" con 3 inputs de alternativas por defecto
-    setCards([...cards, { type: 'paso', content: '', alternatives: [{ text:'', correct: false},{ text: '', correct: false}] }]);
+    setCards([...cards, { type: 'paso', title: '', question: '', expression: '', summary: '', successMessage: '', trueOption: false, falseOption: false, isEditingExpression: false }]);
   };
 
-
-  const handleCardContentChange = (index, field,newContent) => {
+  const handleCardContentChange = (index, field, newContent) => {
     const updatedCards = [...cards];
     updatedCards[index][field] = newContent;
     setCards(updatedCards);
   };
 
+  const handleOpenLatexModal = (index) => {
+    setCurrentCardIndex(index);
+    onOpen();
+  };
+
+  const insertLatex = (command) => {
+    if (currentCardIndex !== null) {
+      const updatedCards = [...cards];
+      const textBefore = updatedCards[currentCardIndex].expression || '';
+      updatedCards[currentCardIndex].expression = textBefore + command;
+      setCards(updatedCards);
+    }
+  };
+
   const handleOptionChange = (index, option) => {
     const updatedCards = [...cards];
-    if (option === 'trueOption'){
-        updatedCards[index].trueOption = !updatedCards[index].trueOption;
-        updatedCards[index].falseOption = false;
-    } else if (option === 'falseOption'){
-        updatedCards[index].falseOption = !updatedCards[index].falseOption;
-        updatedCards[index].trueOption = false;
+    if (option === 'trueOption') {
+      updatedCards[index].trueOption = !updatedCards[index].trueOption;
+      updatedCards[index].falseOption = false;
+    } else if (option === 'falseOption') {
+      updatedCards[index].falseOption = !updatedCards[index].falseOption;
+      updatedCards[index].trueOption = false;
     }
     setCards(updatedCards);
   };
-  
+
   const removeCard = (index) => {
     const updatedCards = cards.filter((_, i) => i !== index);
     setCards(updatedCards);
-};   
-
-  const getCardColor = (type) => {
-    switch (type) {
-      case 'enunciado':
-        return 'blue.200'; // Color para "Enunciado"
-      case 'paso':
-        return 'green.200'; // Color para "Paso"
-      default:
-        return 'gray.200'; // Color por defecto
-    }
-  };
-
-  const getCardLabel = (type) => {
-    switch (type) {
-      case 'enunciado':
-        return 'Enunciado';
-      case 'paso':
-        return 'Paso';
-      default:
-        return 'Desconocido';
-    }
-  };
-
-  const saveData = () => {
-    alert('Datos a guardar:\n' + 'Tarjetas: ' + JSON.stringify(cards));
   };
 
   return (
-    <>
+    <MathJaxContext>
       <div>
         <Flex direction="column" align="center" justify="center" minH="100vh" p={4}>
-          {/* Renderiza las tarjetas */}
           {cards.map((card, index) => (
             <Flex
               key={index}
@@ -76,116 +72,181 @@ export default function newExercise() {
               maxW="800px"
               p={6}
               borderRadius="md"
-              bg={getCardColor(card.type)}
+              bg={card.type === 'enunciado' ? 'blue.200' : 'green.200'}
               boxShadow="md"
               alignItems="center"
               direction="row"
               position="relative"
             >
-              {/* Texto del tipo de tarjeta, rotado -90 grados y posicionado a la izquierda */}
               <Text
                 position="absolute"
-                left="-10px"  // Ajusta este valor para la distancia horizontal
-                top="50%"     // Coloca el texto en el centro verticalmente
-                transform="translateY(-50%) rotate(-90deg)"  // Centra el texto verticalmente y lo rota
-                transformOrigin="left bottom"  // Mantiene la rotación desde la esquina inferior izquierda
+                left="-10px"
+                top="50%"
+                transform="translateY(-50%) rotate(-90deg)"
+                transformOrigin="left bottom"
                 fontSize="lg"
                 fontWeight="bold"
               >
-                {getCardLabel(card.type) +" "+ index}
+                {card.type === 'enunciado' ? 'Enunciado' : 'Paso'} {index}
               </Text>
 
               <Box flex="1" w="100%">
-                {/* Tarjeta de tipo "enunciado" */}
                 {card.type === 'enunciado' ? (
-                  <>
-                    <Input
-                      placeholder={`Contenido del enunciado`}
-                      value={card.content}
-                      onChange={(e) => handleCardContentChange(index, e.target.value)}
-                      bg="white"
-                    />
-                  </>
+                  <Input
+                    placeholder={`Contenido del enunciado`}
+                    value={card.content}
+                    onChange={(e) => handleCardContentChange(index, 'content', e.target.value)}
+                    bg="white"
+                  />
                 ) : (
                   <>
-                    {/* Cuadro de texto dentro de la tarjeta tipo "paso" */}
                     <Input
-                      placeholder="Titulo del paso"
+                      placeholder="Título del paso"
                       value={card.title}
-                      onChange={(e) => handleCardContentChange(index,'title',e.target.value)}
+                      onChange={(e) => handleCardContentChange(index, 'title', e.target.value)}
                       bg="white"
                       mb={2}
                     />
                     <Input
-                      placeholder={`Pregunta del paso`}
+                      placeholder="Pregunta del paso"
                       value={card.question}
-                      onChange={(e) => handleCardContentChange(index, 'question',e.target.value)}
+                      onChange={(e) => handleCardContentChange(index, 'question', e.target.value)}
                       bg="white"
                       mb={2}
-                    />
-                    <Input
-                      placeholder={`Expresion del paso`}
-                      value={card.expression}
-                      onChange={(e) => handleCardContentChange(index, 'expression',e.target.value)}
-                      bg="white"
-                      mb={4}
                     />
 
-                    <Box mb={4} p={4} bg="yellow.100" borderRadius="md" boxShadow="md">
-                        <Text fontWeight="bold">
-                            Verdadero y Falso
-                        </Text>
-                        <Flex align="center" mb={4}>
-                            <Button
-                                colorScheme={card.trueOption ? 'green' : 'gray'}
-                                onClick={() => handleOptionChange(index, 'trueOption')}
-                                mr={2}
-                            >
-                                Verdadero
-                            </Button>
-                            <Button
-                                colorScheme={card.falseOption ? 'red' : 'gray'}
-                                onClick={() => handleOptionChange(index, 'falseOption')}
-                            >
-                                Falso
-                            </Button>
-                        </Flex>
+                    {/* Input de expresión con LaTeX */}
+                    <Box display="flex" alignItems="center" w="100%" mb={2}>
+                      {card.isEditingExpression ? (
+                        <Input
+                          id={`latex-input-${index}`}
+                          value={card.expression}
+                          onChange={(e) => handleCardContentChange(index, 'expression', e.target.value)}
+                          onBlur={() => handleCardContentChange(index, 'isEditingExpression', false)}
+                          placeholder="Expresión del paso"
+                          bg="white"
+                          size="md"
+                          autoFocus
+                        />
+                      ) : (
+                        <Box
+                          p={2}
+                          borderWidth="1px"
+                          borderRadius="md"
+                          w="100%"
+                          h="40px"
+                          bg="white"
+                          onClick={() => handleCardContentChange(index, 'isEditingExpression', true)}
+                          cursor="pointer"
+                          color={card.expression ? "black" : "gray.400"}
+                        >
+                          {card.expression ? (
+                            <MathJax dynamic inline>
+                              {`\\(${card.expression}\\)`}
+                            </MathJax>
+                          ) : (
+                            <Text opacity={0.5}>Expresión del paso</Text>
+                          )}
+                        </Box>
+                      )}
+                      <Button size="xs" ml={2} onClick={() => handleOpenLatexModal(index)}>
+                        ?
+                      </Button>
                     </Box>
-                    
+
+                    {/* Modal de ayuda con LaTeX */}
+                    <Modal isOpen={isOpen} onClose={onClose} size="xl">
+                      <ModalOverlay />
+                      <ModalContent>
+                        <ModalHeader>Ayuda con LaTeX</ModalHeader>
+                        <ModalCloseButton />
+                        <ModalBody>
+                          {currentCardIndex !== null && (
+                            <>
+                              <Input
+                                id={`latex-input-${currentCardIndex}`}
+                                value={cards[currentCardIndex].expression}
+                                onChange={(e) => handleCardContentChange(currentCardIndex, 'expression', e.target.value)}
+                                mb={3}
+                              />
+
+                              <Flex wrap="wrap" gap={2}>
+                                {operations.map((operation, idx) => (
+                                  <Button key={idx} onClick={() => insertLatex(operation.command)} size="sm">
+                                    <MathJax dynamic inline>{`\\(${operation.label}\\)`}</MathJax>
+                                  </Button>
+                                ))}
+                              </Flex>
+
+                              <Box mt={4} p={2} borderWidth="1px" borderRadius="md">
+                                <MathJax dynamic inline>
+                                  {`\\(${cards[currentCardIndex].expression}\\)`}
+                                </MathJax>
+                              </Box>
+                            </>
+                          )}
+                        </ModalBody>
+
+                        <ModalFooter>
+                          <Button colorScheme="blue" mr={3} onClick={onClose}>
+                            Cerrar
+                          </Button>
+                        </ModalFooter>
+                      </ModalContent>
+                    </Modal>
+
+                    <Box mb={4} p={4} bg="yellow.100" borderRadius="md" boxShadow="md">
+                      <Text fontWeight="bold">Verdadero y Falso</Text>
+                      <Flex align="center" mb={4}>
+                        <Button
+                          colorScheme={card.trueOption ? 'green' : 'gray'}
+                          onClick={() => handleOptionChange(index, 'trueOption')}
+                          mr={2}
+                        >
+                          Verdadero
+                        </Button>
+                        <Button
+                          colorScheme={card.falseOption ? 'red' : 'gray'}
+                          onClick={() => handleOptionChange(index, 'falseOption')}
+                        >
+                          Falso
+                        </Button>
+                      </Flex>
+                    </Box>
+
                     <Input
-                      placeholder={`Summary del paso`}
+                      placeholder="Summary del paso"
                       value={card.summary}
-                      onChange={(e) => handleCardContentChange(index, 'summary',e.target.value)}
+                      onChange={(e) => handleCardContentChange(index, 'summary', e.target.value)}
                       bg="white"
                       mb={4}
                     />
                     <Input
-                      placeholder={`Mensaje de paso correcto`}
+                      placeholder="Mensaje de paso correcto"
                       value={card.successMessage}
-                      onChange={(e) => handleCardContentChange(index, 'successMessage',e.target.value)}
+                      onChange={(e) => handleCardContentChange(index, 'successMessage', e.target.value)}
                       bg="white"
                       mb={4}
                     />
                     <Button colorScheme="red" onClick={() => removeCard(index)}>
-                        Eliminar paso
+                      Eliminar paso
                     </Button>
                   </>
                 )}
               </Box>
             </Flex>
           ))}
-        
-          {/* Botón para agregar una nueva tarjeta (solo tarjetas de tipo "paso") */}
+
           <Stack spacing={4} direction="row" align="center">
             <Button onClick={addCard} alignSelf="center">
               Agregar tarjeta tipo paso
             </Button>
-            <Button colorScheme="green" onClick={saveData} alignSelf="center">
+            <Button colorScheme="green" onClick={() => alert('Datos guardados!')}>
               Guardar Cambios
             </Button>
           </Stack>
         </Flex>
       </div>
-    </>
+    </MathJaxContext>
   );
 }
